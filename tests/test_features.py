e234b9585e6bb1cc45dd97acf5629e50983fbc17
Miłosz Taracha <milosztaracha@MacBook-Pro-Miosz.local>
2023-05-21 21:23:56 +0200

    add a bunch of things

---
+++ tests/test_features.py
@@ -0,0 +1,10 @@
+from istar.data.features import add_cumulative_sums, add_rolling_means
+import pytest
+
+
+def test_add_rolling_means():
+    pass
+
+
+def test_add_cumulative_features():
+    pass
